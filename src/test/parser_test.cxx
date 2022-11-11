0167eda4f37ded15ff45a7f75343645b7e547dd5
Sam Beer <sam.beer208@gmail.com>
2022-11-11 12:14:15 +0100

    test_bool_expr example test

--- src/test/parser_test.cxx
+++ src/test/parser_test.cxx
@@ -6,13 +6,38 @@
 #include "test_utils.hxx"
 #include "parser.hxx"
 
+int check (std::unique_ptr<bassoon::ExprAST> result){
+    if(!result){
+        fprintf(stderr,"FAILED\n");
+        return 1;
+    }
+    fprintf(stderr,"pass");
+    return 0;
+}
+
 namespace bassoon{
 namespace test{
+
+
+int test_bool_expr(){
+    fprintf(stderr,"test_bool_expr\n");
+    utils::setup_input_string("true");
+    std::unique_ptr<ExprAST> result = Parser::_testParseExpression();
+    return check(std::move(result));
+}
+int test_int_expr();
+int test_double_expr();
+
+int test_paren_expr();
+
+int test_binop_expr();
+
+int test_call_expr();
+
     
 int test_parser(){
-    utils::setup_input_string("(a+a)");
     utils::setupSource();
-    Parser::_testParseExpression();
+    test_bool_expr();
     return 0;
 }
 
