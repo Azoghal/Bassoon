8a333a0daf76a34c7f1235d2625158cc275eb45c
Sam Beer <sam.beer208@gmail.com>
2022-11-18 16:59:58 +0100

    calling viz_visitor

--- src/bassoon.cxx
+++ src/bassoon.cxx
@@ -1,13 +1,15 @@
 #include "lexer.hxx"
 #include "parser.hxx"
 #include "tokens.hxx"
-#include "astviz.hxx"
+#include "viz_visitor.hxx"
 #include <iostream>
 
 int main(int argc, char *argv[]){
     bassoon::Parser::setVerbosity(1);
     std::vector<std::unique_ptr<bassoon::NodeAST>> ASTs;
     bassoon::Parser::parseLoop(std::move(ASTs));
-    
+    bassoon::viz::VizVisitor * visualiser = new bassoon::viz::VizVisitor;
+    visualiser->visualiseAST(std::move(ASTs[ASTs.size()-1]));
+    ASTs.pop_back();
     return 0;
 }
