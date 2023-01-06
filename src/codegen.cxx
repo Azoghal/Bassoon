45184a83c9ac325676609441ab157db018a2808e
Sam Beer <sam.beer208@gmail.com>
2023-01-06 14:57:43 +0000

    test codegen setu

--- src/codegen.cxx
+++ src/codegen.cxx
@@ -1,6 +1,24 @@
 #include "ast.hxx"
+#include "codegen.hxx"
 
 namespace bassoon
 {
+namespace codegen
+{
+
+CodeGenerator::CodeGenerator(){
+    context_ = std::make_unique<llvm::LLVMContext>();
+    module_ = std::make_unique<llvm::Module>("doubleReed", *context_);
+    builder_ = std::make_unique<llvm::IRBuilder<>>(*context_);
+}
+
+void CodeGenerator::MakeTestIR(){
     
+}
+
+void CodeGenerator::PrintIR(){
+    module_->print(llvm::errs(), nullptr);
+}
+
+} // namespace codegen
 } // namespace bassoon
