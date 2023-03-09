ec5072019972d2e4b63223945c1c47a63671f2b0
Sam Beer <sam.beer208@gmail.com>
2023-03-09 23:49:05 +0000

    visitor logging

--- include/log_error.hxx
+++ include/log_error.hxx
@@ -1,11 +1,13 @@
 #ifndef Bassoon_include_log_error_HXX
 #define Bassoon_include_log_error_HXX
 
+#include "spdlog/spdlog.h"
+
 namespace bassoon {
 
 template<class T> 
 T LogError(std::string str){
-    fprintf(stderr, "Error: %s\n", str.c_str());
+    spdlog::error("Error: {0}", str);
     return nullptr;
 }
 
